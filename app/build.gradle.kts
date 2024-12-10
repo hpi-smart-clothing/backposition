1ebd0bbf19ae0c67af19d17d34424e2c1f11404f
nu444 <nils1@posteo.de>
2024-12-10 12:52:43 +0100

    fixed bluetooth pairing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "de.bp.backposition"
-        minSdk = 28
+        minSdk = 34
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/de/bp/backposition/MainActivity.kt
+++ app/src/main/java/de/bp/backposition/MainActivity.kt
@@ -1,6 +1,7 @@
 package de.bp.backposition
 import android.Manifest
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
 import android.bluetooth.le.ScanSettings
 import android.content.pm.PackageManager
 import android.os.Bundle
@@ -30,28 +31,14 @@ import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberPermissionState
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.text.style.TextAlign
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.flowWithLifecycle
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.launch
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.core.app.ActivityCompat
-import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.accompanist.permissions.shouldShowRationale
 import com.xsens.dot.android.sdk.models.DotDevice
 import kotlin.contracts.contract
 
 class MainActivity : ComponentActivity(), DotDeviceCallback, DotScannerCallback {
+    val discoveredDots = ArrayList<String>()
+    val connectedDots = ArrayList<DotDevice>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DotSdk.setDebugEnabled(true)
@@ -76,7 +63,7 @@ class MainActivity : ComponentActivity(), DotDeviceCallback, DotScannerCallback 
     @Composable
     fun RequestBluetoothPermission() {
         Log.d("BT", "Start Looking")
-        val permissions = listOf(Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_CONNECT, Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.ACCESS_FINE_LOCATION)
+        val permissions = listOf(Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_CONNECT, Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
         val bluetoothPermissionState = rememberMultiplePermissionsState(
             permissions = permissions
         )
@@ -96,70 +83,85 @@ class MainActivity : ComponentActivity(), DotDeviceCallback, DotScannerCallback 
     }
 
     override fun onDotConnectionChanged(p0: String?, p1: Int) {
-        Log.d("BT", "Connection Changed")
+        Log.d("BT", "Connectionnnnn Changed"+ p0)
     }
 
-    override fun onDotServicesDiscovered(p0: String?, p1: Int) {
-        TODO("Not yet implemented")
+    override fun onDotServicesDiscovered(address: String?, status: Int) {
+        if (status == BluetoothGatt.GATT_SUCCESS){
+            Log.d("BT", "Services Discovered")
+        }
     }
 
-    override fun onDotFirmwareVersionRead(p0: String?, p1: String?) {
-        TODO("Not yet implemented")
+    override fun onDotFirmwareVersionRead(address: String?, version: String?) {
+        Log.d("BT", "Firmware Version:" + version)
     }
 
     override fun onDotTagChanged(p0: String?, p1: String?) {
-        TODO("Not yet implemented")
+        Log.d("BT", "Tag Changed")
     }
 
     override fun onDotBatteryChanged(p0: String?, p1: Int, p2: Int) {
-        TODO("Not yet implemented")
+        Log.d("BT", "Battery Changed")
     }
 
     override fun onDotDataChanged(p0: String?, p1: DotData?) {
-        TODO("Not yet implemented")
+        Log.d("BT", "Data Changed")
     }
 
     override fun onDotInitDone(p0: String?) {
-        TODO("Not yet implemented")
+        Log.d("BT", "Init Done")
+        Log.d("BT", "--------FIRST DATA--------")
+        Log.d("BT", connectedDots[0].firmwareVersion)
+        Log.d("BT", "Battery: " + connectedDots[0].batteryPercentage)
+        connectedDots[0].identifyDevice()
+
     }
 
     override fun onDotButtonClicked(p0: String?, p1: Long) {
-        TODO("Not yet implemented")
+        Log.d("BT", "Button Clicked")
     }
 
     override fun onDotPowerSavingTriggered(p0: String?) {
-        TODO("Not yet implemented")
+        Log.d("BT", "Power Saving Triggered")
     }
 
     override fun onReadRemoteRssi(p0: String?, p1: Int) {
-        TODO("Not yet implemented")
+        Log.d("BT", "Read Remote Rssi")
     }
 
     override fun onDotOutputRateUpdate(p0: String?, p1: Int) {
-        TODO("Not yet implemented")
+        Log.d("BT", "Output Rate Update")
     }
 
     override fun onDotFilterProfileUpdate(p0: String?, p1: Int) {
-        TODO("Not yet implemented")
+        Log.d("BT", "Filter Profile Update")
     }
 
     override fun onDotGetFilterProfileInfo(p0: String?, p1: ArrayList<FilterProfileInfo>?) {
-        TODO("Not yet implemented")
+        Log.d("BT", "Get Filter Profile Info")
     }
 
     override fun onSyncStatusUpdate(p0: String?, p1: Boolean) {
-        TODO("Not yet implemented")
+        Log.d("BT", "Sync Status Update")
     }
 
     override fun onDotScanned(p0: BluetoothDevice, p1: Int) {
         val name = p0.name
         val address = p0.address
-        val newDot = DotDevice(applicationContext, p0, this)
-        Log.d("BT", newDot.toString())
-        Log.d("BT", "Found Device: $name")
-        if (name != null) {
-            newDot.connect()
-            Log.d("BT", "Connected to: $name")
+        if (address != null) {
+            if(!discoveredDots.contains(address)){
+                discoveredDots.add(address)
+                val newDot = DotDevice(applicationContext, p0, this)
+                Log.d("BT", newDot.toString())
+                Log.d("BT", "Found Device: $name")
+                if (name != null) {
+                    newDot.connect()
+                    connectedDots.add(newDot)
+                    Log.d("BT", "Connected to: $name")
+                }
+            }else{
+                Log.d("BT", "Device $name already discovered")
+            }
         }
     }
 }
